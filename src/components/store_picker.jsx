1699c8bd26771f5d55cd99351d2a9cac9fe3dc58
claire froelich <claire.froelich@gmail.com>
2020-01-19 15:46:21 -0500

    implement routing with store picker

--- src/components/store_picker.jsx
+++ src/components/store_picker.jsx
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
 
 class StorePicker extends Component {
+  // syntax for explicitly this-binding methods (if not using arrow function)
+  // constructor() {
+  //   super();
+  //   this.goToStore = this.goToStore.bind(this);
+  // }
+
+  myInput = React.createRef();
+
+  goToStore = (e) => {
+    e.preventDefault(); //stop page reload on form submit
+    // grab text from myInput
+    const storeName = this.myInput.current.value;
+    // change url with push state (change without refresh/losing memory)
+    this.props.history.push(`/store/${storeName}`)
+  }
+
   render() {
     return (
-       <form action="" className="store-selector">
+       <form  className="store-selector" onSubmit={this.goToStore}>
         <h2>Enter A Store</h2>
-        <input type="text" required placeholder="Store name"/>
+        <input
+          type="text"
+          ref={this.myInput}
+          required
+          placeholder="Store name"/>
         <button type="submit">Visit Store ></button>
        </form>
     );
